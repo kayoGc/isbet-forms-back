3a8a466a324eef912333f6b3c78e320ca3ff6b9c
kayoGc <kayivicti@gmail.com>
2025-09-25 16:42:19 -0300

    typo

--- src/config/db.js
+++ src/config/db.js
@@ -8,7 +8,7 @@ const connectDb = async () => {
         // tenta fazer a conexão
         await mongoose.connect(process.env.DB_URI);
 
-        console.log("\nBanco de dados conectado com sucesso conectado com sucesso");
+        console.log("\nBanco de dados conectado com sucesso");
     } catch (err) {
         // caso a conexão falhar
         console.error("Erro conectando com o banco de dados:", err.message);
